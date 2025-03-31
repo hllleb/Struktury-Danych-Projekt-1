74632d05c1185ec951dd842a5f0b559831a157ed
Hleb Lizhbanau <gleblizhbanov@gmail.com>
2025-03-31 23:59:24 +0200

    Add Doxygen-style comments, fix a typo

--- DynamicArray.h
+++ DynamicArray.h
@@ -8,19 +8,26 @@ namespace DataStructures
     const int DefaultCapacity = 4;
     const int CapacityMultiplier = 2;
 
+    /// \brief Implements a dynamic array
+    /// \tparam T
     template<typename T>
     class DynamicArray
     {
     public:
+        /// \brief Constructs an empty array with the capacity of 4 elements
         DynamicArray() : DynamicArray<T>(DefaultCapacity)
         {
         }
 
+        /// \brief Constructs an empty array with the capacity of \p capacity elements.
+        /// \param capacity Capacity of the array
         explicit DynamicArray(int capacity) : capacity(capacity), length(0)
         {
             this->items = new T[this->capacity];
         }
 
+        /// \brief Constructs an array containing the elements from the \p initializerList
+        /// \param initializerList An \a initializer_list containing elements for the array
         DynamicArray(const std::initializer_list<T> &initializerList) : DynamicArray(initializerList.size())
         {
             for (const auto &item: initializerList)
@@ -29,34 +36,48 @@ namespace DataStructures
             }
         }
 
-        DynamicArray(const DynamicArray<T> &list) : capacity(list.capacity), length(list.length)
+        /// \brief Constructs an \p array deep copy
+        /// \param array An array to copy elements from
+        DynamicArray(const DynamicArray<T> &array) : capacity(array.capacity), length(array.length)
         {
             this->items = new T[this->capacity];
-            Copy(list.items, 0, this->items, 0, list.length);
+            Copy(array.items, 0, this->items, 0, array.length);
         }
 
+        /// \brief Destructs the array freeing up the memory used for storing its elements
         ~DynamicArray()
         {
             delete[] this->items;
         }
 
+        /// \brief Returns the current number of elements in the array
+        /// \return Number of elements in the array
         int GetLength() const
         {
             return this->length;
         }
 
+        /// \brief Accesses the element at given \p index position in the array
+        /// \param index An zero-based index of an array item
+        /// \return An element at the given \p index position
         T operator[](int index) const
         {
             CheckIndex(index);
             return this->items[index];
         }
 
+        /// \brief Accesses the element at given \p index position in the array
+        /// \param index An zero-based index of an array item
+        /// \return An element at the given \p index position
         T &operator[](int index)
         {
             CheckIndex(index);
             return this->items[index];
         }
 
+        /// \brief Performs a deep copying of the \p array and assigns its contents to the current object
+        /// \param array An array to copy from
+        /// \return A reference to the edited object
         DynamicArray<T> &operator=(const DynamicArray<T> &array)
         {
             if (&array == this)
@@ -76,6 +97,9 @@ namespace DataStructures
             return *this;
         }
 
+        /// \brief Performs a deep copying of the \p initializerList and assigns its contents to the current object
+        /// \param array An \a initializer_list to copy from
+        /// \return A reference to the edited object
         DynamicArray<T> &operator=(const std::initializer_list<T> &initializerList)
         {
             if (this->capacity < initializerList.size())
@@ -107,6 +131,8 @@ namespace DataStructures
             return *this;
         }
 
+        /// \brief Adds a new \p item to the array
+        /// \param item An item to add
         void Add(T item)
         {
             if (this->length == this->capacity)
@@ -117,6 +143,9 @@ namespace DataStructures
             this->items[this->length++] = item;
         }
 
+        /// \brief Inserts a new \p item to the given \p index of the array
+        /// \param index A zero-based index in array, where new \p item should be inserted
+        /// \param item An item to insert
         void Insert(int index, T item)
         {
             CheckIndex(index);
@@ -143,6 +172,9 @@ namespace DataStructures
             this->length++;
         }
 
+        /// \brief Verifies if the \p item is present in the array
+        /// \param item An item to search for
+        /// \return \a true if the item was found, \a false otherwise
         bool Contains(T item) const
         {
             for (int i = 0; i < this->length; i++)
@@ -156,6 +188,9 @@ namespace DataStructures
             return false;
         }
 
+        /// \brief Searches the array for the first occurrence of the given \p item
+        /// \param item An item to search for
+        /// \return A zero-based index of the first occurrence of \p item in the array if found, -1 otherwise
         int IndexOf(T item) const
         {
             for (int i = 0; i < this->length; i++)
@@ -169,6 +204,9 @@ namespace DataStructures
             return -1;
         }
 
+        /// \brief Searches the array for the last occurrence of the given \p item
+        /// \param item An item to search for
+        /// \return A zero-based index of the last occurrence of \p item in the array if found, -1 otherwise
         int LastIndexOf(T item) const
         {
             for (int i = this->length - 1; i >= 0; i--)
@@ -182,11 +220,14 @@ namespace DataStructures
             return -1;
         }
 
+        /// \brief Removes all the items from the array
         void Clear()
         {
             this->length = 0;
         }
 
+        /// \brief Removes an array item at the given \p index
+        /// \param index A zero-based index of the element to remove
         void RemoveAt(int index)
         {
             CheckIndex(index);
@@ -199,11 +240,13 @@ namespace DataStructures
             --this->length;
         }
 
+        /// \brief Removes the first item of the array.
         void RemoveFirst()
         {
             return RemoveAt(0);
         }
 
+        /// \brief Removes the last item of the array.
         void RemoveLast()
         {
             this->length--;
